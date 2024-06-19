64bc0caa4ee790fafb4a10ac38e7464c9d284a81
KharchenkoValery <harchenkovalerochka@gmail.com>
2024-06-19 13:14:21 +0600

    DZ3_Valery_38-01

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -47,28 +47,277 @@ const regExp = /@gmail.com$/;
 //    }
 //    counter()
 
-let position = 0;
+let positionLeft = 0, positionTop = 0;
+let positionRight = 0, positionBottom = 0;
+let child_block = document.querySelector(".child_block");
 const parent_block = document.querySelector(".parent_block");
-let width = parent_block.style.width = 448;
-function blockAnimation () {
-    position++;
-    if (position > width) return;
-      document.querySelector(".child_block").style.left = position + 'px';  
-      animation(); 
-}
-blockAnimation();
-
-function animation () {
-    requestAnimationFrame(blockAnimation);
-}
-animation();
+const width = parent_block.offsetWidth -= child_block.offsetWidth;
+const height = parent_block.offsetHeight -= child_block.offsetHeight;
 
 
-    // let days = 0;
-    // function howMuchToLearnJS() {
-    //     if (days === 400) return; // условие выхода
-    //     days++;
-    //     console.log(days);
-    //     howMuchToLearnJS();
+// function blockAnimation () {
+   
+//     if (positionLeft <= width) {
+//         positionLeft++;   
+//        child_block.style.left = `${positionLeft}px`;  
+//        animation()
+//     } else if (positionLeft >= width ) {
+      
+//         positionRight--;
+//         console.log("smfslg");
+//         child_block.style.right= `${positionRight}px`;
+//         noAnimation()
+//     }
+   
+// }  
+// function animation () {
+//   requestAnimationFrame(blockAnimation)
+// }
+//     //  animation()
+// // } 
+// function noAnimation () {
+//   // clearInterval (animation)
+//   requestAnimationFrame(blockAnimation)
+// }
+// noAnimation()
+
+
+// function animation () {
+//     if (positionLeft >= width) {
+//         blockAnimationLeft = !blockAnimationLeft
+//     }
+// }
+// animation()
+
+function blockAnimationLeft() {
+                if (positionLeft <= width) {
+                positionLeft++;
+                
+                console.log (positionLeft)
+                child_block.style.left = `${positionLeft}px`;  
+                positionLeft.disabled = false;
+                animationLeft();
+        } }
+      function  animationLeft() {
+        setInterval(blockAnimationLeft, 100)             
+        } 
+        // animationLeft()
+        function blockAnimationRight() {
+           positionLeft.disabled = true;
+                    if (positionLeft >= width && positionRight < width) {
+                    positionRight--;
+                   
+                    console.log (positionLeft)
+                    child_block.style.right = `${positionRight}px`;  
+                    animationRight();
+            } }
+          //  function interval() {
+          //   setInterval(blockAnimationRight, 10)
+          //  }     
+        //   function  animationRight() {
+        //     setTimeout(interval, 8000 )
+        // } 
+            // animationRight()
+              function  animationRight() {
+                  setInterval(blockAnimationRight, 100)
+                }
+                  animationRight()
+//  function blockAnimationLeft() {
+//     // for (i=0; i++) {
+//         // for (let i; i=0; i <= width) {
+        
+//             if (positionLeft <= width) {
+//         // while(positionLeft < width && positionTop < height){
+//             positionLeft++;
+//             positionLeft.disabled = false;
+//             // positionTop++;
+//             console.log (positionLeft)
+//             child_block.style.left = `${positionLeft}px`;  
+//             // child_block.style.top = `${positionTop}px`;
+//             // i = null;
+//             // noAnimation()
+//             animationLeft();
+//             // noAnimation ();
+//         // }
+
+//     } 
+//     else if (positionLeft >= width && positionRight < width) {
+//         positionLeft.disabled = true;
+//         positionRight++;
+//         // console.log(positionLeft);
+//         console.log(positionRight);
+//         document.querySelector(".child_block").style.right = `${positionRight}px`;
+           
+//          animationTop()     
+//     }
+//         // }
+          
+//         positionLeft.disabled = true;
+     
+//     }
+  
+    
+//     // }
+// function animationLeft () {
+//     setTimeout(blockAnimationLeft, 10);
+//  }   
+// animationLeft ()
+
+
+
+
+// function noAnimation () {
+//     setTimeout(() => {
+//       clearInterval(blockAnimationLeft)  
+//     }, 5000)
+    
+// }
+
+// noAnimation()
+// function blockAnimation () {
+   
+
+//     function   blockAnimationTop () {
+            
+//         if (positionTop < width && positionLeft >= width ) {
+//             // blockAnimationLeft() = !blockAnimationLeft();
+//             i = false;
+//             positionTop++;
+//             console.log(positionTop);
+//            document.querySelector(".child_block").style.right = `${positionTop}px`;
+//         // animationTop();   
+//         };
+               
+//     }
+// // }
+
+
+
+//  function animationTop () {
+//     requestAnimationFrame(blockAnimationLeft);
+// }
+//  animationTop ()
+
+
+
+    // else if(positionLeft >= width && positionTop >= height){
+    //     while(positionLeft >= width && positionTop >= height){
+    //         positionTop--;
+    //         positionLeft--;
+    //         console.log(positionTop);
+    //         console.log(positionLeft);
+    //         child_block.style.left = `${positionLeft}px`;
+    //         child_block.style.top = `${positionTop}px`;
+            
+    //     }
+    //     animation(); 
     // }
-    // howMuchToLearnJS();
+
+ 
+// blockAnimation();
+
+
+
+
+// setInterval(function request() {
+//     blockAnimation();
+// },10);
+
+
+
+// animation ()
+
+
+//
+
+// setTimeout(animationTop, 2000);
+// setTimeout ()
+// animationTop ();
+
+
+// function timer () {
+//     // second++;
+//     start.addEventListener('click') 
+//         second.innerHTML = 1;
+//     }
+
+ 
+
+// start.addEventListener('click', function printNumbers(from, to) {
+//    let current = from;
+
+//    let timerId = setInterval(function() {
+//     innerHTML = current;
+//     if (current == to) {
+//    clearInterval(start);
+//    }
+//    second++;
+    
+// }, 1000)
+// })
+
+
+
+//  printNumbers(5, 10);
+
+
+// let n = 1;
+// let i= 0;
+// function red () {
+//     stop.innerHTML = sum;
+// setInterval(red, 1000);
+// }setInterval (red, 1000);
+
+    // i++
+//    let sum = setTimeout(i+n, 1000);
+
+// setTimeout (timeout, 1000);
+
+// function timeout () {
+//     setInterval(red, 1000);
+// }
+// setTimeout (timeout, 1000);
+
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
+let second = document.querySelector("#seconds");
+
+start.addEventListener('click', function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      second.innerHTML= current;
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+    printNumbers(0, 10);
+  }
+)
+
+stop.addEventListener( 'click', function stopNumbers() {
+    clearInterval(printNumbers);
+})
+
+
+// persons
+
+const getDataBtn = document.querySelector(".personBtn");
+
+getDataBtn.onclick = () => {
+  const request = new XMLHttpRequest(); // 1. Создание запроса
+  request.open('DATA', '../data/persons.json') // 2. Указание метода запроса и указание пути
+  // 3.
+
+}
+
+  
+
+  
+
+
+
+
+    
--- project/js/lesson.js
+++ project/js/lesson.js
@@ -13,3 +13,51 @@ phoneButton.onclick = () => {
         phoneSpan.style.color = "red"
     }
 }
+
+// 
+
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabContentItems = document.querySelectorAll(".tab_content_item");
+const tabParent = document.querySelector(".tab_content_items");
+
+const hideTabContent = ()  => {
+    tabContentBlocks.forEach((item) => {
+    item.style.display = 'none';
+})
+tabContentItems.forEach((item) => {
+    item.classList.remove("tab_content_item_active")
+})
+}
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block';
+    tabContentItems[index].classList.add("tab_content_item_active");
+}
+hideTabContent();
+showTabContent();
+
+tabParent.onclick = (event) => {
+    if (event.target.classList.contains("tab_content_item")) {
+        tabContentItems.forEach((item, index) => {
+            if(event.target === item) {
+                hideTabContent();
+                showTabContent(index);
+                
+            }
+        })
+    }
+}
+
+// auto tab
+let index = 0;
+const autoTabContent = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > tabContentBlocks.length - 1) {
+            i = 0
+        }
+        hideTabContent();
+        showTabContent(i);
+    }, 3000)
+    
+}
+autoTabContent(index);
